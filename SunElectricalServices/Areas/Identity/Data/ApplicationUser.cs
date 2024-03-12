a3923675d162f2a087c6fbbfac6ddc9e0a91e441
Sohar Palapati <soharpalapati@gmail.com>
2024-03-13 10:10:31 +1300

    Added Application User and made Register and Login buttons work

---
+++ SunElectricalServices/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿namespace SunElectricalServices.Areas.Identity.Data
+{
+    public class ApplicationUser
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+    }
+}
