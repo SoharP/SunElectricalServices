571809186a04b98ab510c555e470bb3afd1f2283
sohar <ac116496@avcol.school.nz>
2024-04-09 10:10:38 +1200

    Added Book a service page

--- SunElectricalServices/Controllers/HomeController.cs
+++ SunElectricalServices/Controllers/HomeController.cs
@@ -63,6 +63,11 @@ namespace SunElectricalServices.Controllers
             return View();
         }
 
+        public IActionResult Book()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
