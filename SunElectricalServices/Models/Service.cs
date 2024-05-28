3f39a344356fb6286ee5d75abc9d3f2f8212d0b6
Sohar Palapati <ac116496@avcol.school.nz>
2024-05-29 10:10:20 +1200

    Added display names

--- SunElectricalServices/Models/Service.cs
+++ SunElectricalServices/Models/Service.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 
 namespace SunElectricalServices.Models
 {
@@ -7,10 +8,18 @@ namespace SunElectricalServices.Models
         public int ServiceID { get; set; }
         public string Residential { get; set; }
         public int Commercial { get; set; }
+
+        [DisplayName("HeatPump Installations")]
         public string Heatpump_Installations { get; set; }
         public string EVChargers { get; set; }
+
+        [DisplayName("Switchboard Upgrades")]
         public string Switchboard_Upgrade { get; set; }
+
+        [DisplayName("Smart Home Installations")]
         public string Smarthome_installations { get; set; }
+
+        [DisplayName("Mainainence and Servicing")]
         public string Maintainence_and_Servicing { get; set; }
         public int StaffID { get; set; }
         public Staff Staff { get; set; }
