b4d318a41b1bc1e76a526c0ce3f9638a80d04c53
Sohar Palapati <ac116496@avcol.school.nz>
2024-06-13 15:14:03 +1200

    Added migrations

--- SunElectricalServices/Migrations/20240613023210_SunElectricalServices.Designer.cs
+++ SunElectricalServices/Migrations/20240613023210_SunElectricalServices.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SunElectricalServices.Areas.Identity.Data;
 
@@ -11,9 +12,11 @@ using SunElectricalServices.Areas.Identity.Data;
 namespace SunElectricalServices.Migrations
 {
     [DbContext(typeof(SunContext))]
-    partial class SunContextModelSnapshot : ModelSnapshot
+    [Migration("20240613023210_SunElectricalServices")]
+    partial class SunElectricalServices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
